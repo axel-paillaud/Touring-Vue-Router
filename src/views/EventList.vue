176b034498d08001232d5816823fb12bec56e84f
axel-paillaud <contact@axelweb.fr>
2023-09-08 17:32:17 +0200

    challenge: add pagination number(1, 2, 3...) OK

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,21 +3,71 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
-    <router-link
-      :to="{ name: 'EventList', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-      >Prev Page</router-link
-    >
+    <div class="pagination">
+      <div>
+        <router-link
+          :to="{ name: 'EventList', query: { page: page - 1 } }"
+          rel="prev"
+          id="page-prev"
+          v-if="page != 1"
+          >&#60; Previous</router-link
+        >
+      </div>
 
-    <router-link
-      :to="{ name: 'EventList', query: { page: page + 1 } }"
-      rel="next"
-      v-if="hasNextPage"
-      >Next Page</router-link
-    >
+      <ul class="pagination-list-container">
+        <li v-for="n in totalPages" :key="n">
+          <router-link :to="{ name: 'EventList', query: {page: n} }">{{ n }}</router-link>
+        </li>
+      </ul>
+
+      <div>
+        <router-link
+          :to="{ name: 'EventList', query: { page: page + 1 } }"
+          rel="next"
+          id="page-next"
+          v-if="hasNextPage"
+          >Next &#62;</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+ul {
+  padding: 0;
+  list-style-type: none;
+  margin: 0;
+}
+
+.pagination-list-container {
+  display: flex;
+  gap: 4px;
+}
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+  justify-content: space-between;
+}
+.pagination a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+</style>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
@@ -34,6 +84,7 @@ export default {
     return {
       events: null,
       totalEvents : 0,
+      totalPages : 0,
     }
   },
   created() {
@@ -43,6 +94,7 @@ export default {
       .then(response => {
         this.events = response.data
         this.totalEvents = response.headers['x-total-count']
+        this.totalPages = Math.ceil(this.totalEvents / 2)
 
       })
       .catch(error => {
@@ -52,12 +104,8 @@ export default {
   },
   computed: {
     hasNextPage() {
-      // First, calculate total pages
-      var totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per page
-
-      // Then check to see if the current page is less than the total pages.
-      return this.page < totalPages
-    }
+      return this.page < this.totalPages
+    },
   }
 }
 </script>
